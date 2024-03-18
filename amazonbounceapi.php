e89897c563fecd15ac7c13a37c40832b69827bbc
Jo Franz <franz@systopia.de>
2024-03-18 12:06:22 +0100

    civix upgrade 23.02.1

--- amazonbounceapi.php
+++ amazonbounceapi.php
@@ -22,59 +22,12 @@ function amazonbounceapi_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function amazonbounceapi_civicrm_postInstall() {
-  _amazonbounceapi_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function amazonbounceapi_civicrm_uninstall() {
-  _amazonbounceapi_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function amazonbounceapi_civicrm_enable() {
   _amazonbounceapi_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function amazonbounceapi_civicrm_disable() {
-  _amazonbounceapi_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function amazonbounceapi_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _amazonbounceapi_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function amazonbounceapi_civicrm_entityTypes(&$entityTypes) {
-  _amazonbounceapi_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
